cd4dcd160fa44e4a1249dc3ce6a4df8344bc97dd
vicpasjava <vicpasjava@mail.ru>
2023-05-15 12:50:21 +0300

    04 - add dependency and logic to report dsts to console

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import com.codahale.metrics.*;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 
@@ -11,6 +15,15 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		MetricRegistry metricRegistry = new MetricRegistry();
+		Gauge<Long> gauge = new AttendanceRatioGauge();
+		gauge.getValue();
+		metricRegistry.register("Certificate expiration days left", gauge);
+
+		ConsoleReporter reporter = ConsoleReporter.forRegistry(metricRegistry).build();
+		reporter.start(1, TimeUnit.DAYS);
+		reporter.report();
 	}
 
 	@Bean
